5f96a9b78bae3a6e08b9145bd6b6723000a1c1f5
harry-c26 <>
2023-10-25 15:06:40 +0000

    corrected an error in the progress bar function

--- assets/js/game.js
+++ assets/js/game.js
@@ -112,7 +112,7 @@ getNewQuestion = () => {
 
     questionCounter++;
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-    progressBarFull.getElementsByClassName.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionsIndex];
